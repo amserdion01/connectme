e4b32e9b70905240e7c114e403cc38350d4d0808
marius@crewmeister.com <marius@crewmeister.com>
2023-03-25 20:59:32 +0200

    a

--- src/components/ServerList.tsx
+++ src/components/ServerList.tsx
@@ -4,7 +4,7 @@ import ServerCard from './ServerCard'
 
 const ServerList : React.FC<{filter:string | undefined}>= ({filter}) => {
     const servers = api.server.getAllServers.useQuery({filter})
-
+    
     if (!servers.data) return <div> Loading</div>
     return (
     <div className='w-screen flex flex-wrap'>{servers.data.map((server)=>{ 

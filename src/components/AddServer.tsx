0494ec7591d201c89f1311a5d152ddd0703bada6
marius@crewmeister.com <marius@crewmeister.com>
2023-03-30 22:27:00 +0300

    nice interface, good data

--- src/components/AddServer.tsx
+++ src/components/AddServer.tsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 function AddServer() {
     return (
         <div className="flex justify-center items-center">
-        <div className="w-96">
-          <Link href="/form">
+        <div className="w-fit text-center">
             <div className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
               Add a new server
             </div>
-          </Link>
+
         </div>
       </div>
     );}
